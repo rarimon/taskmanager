0396d899bf87c226e6a01fa15af4643a20f1b599
rarimon <aminulislam221@gmail.com>
2025-07-19 23:26:30 +0600

    add page router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, {Fragment} from 'react';
+import {BrowserRouter, Route, Routes} from "react-router";
+import DashboardPage from "./pages/DashboardPage.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
+import RegistrationPage from "./pages/RegistrationPage.jsx";
+import CreatePage from "./pages/CreatePage.jsx";
+import CanceledPage from "./pages/CanceledPage.jsx";
+import CompletedPage from "./pages/CompletedPage.jsx";
+import NewPage from "./pages/NewPage.jsx";
+import ProfilePage from "./pages/ProfilePage.jsx";
+import ForgetpassPage from "./pages/ForgetpassPage.jsx";
+import Page404 from "./pages/Page404.jsx";
 
 const App = () => {
     return (
-        <div>
-            <h1 className="text-success">sdlkfkls</h1>
-        </div>
+        <Fragment>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<DashboardPage />} />
+                    <Route path="/login" element={<LoginPage />} />
+                    <Route path="/registraion" element={<RegistrationPage />} />
+                    <Route path="/careate" element={<CreatePage />} />
+                    <Route path="/cancel" element={<CanceledPage />} />
+                    <Route path="/complate" element={<CompletedPage />} />
+                    <Route path="/all" element={<NewPage />} />
+                    <Route path="/profile" element={<ProfilePage />} />
+                    <Route path="/forgate" element={<ForgetpassPage />} />
+                    <Route path="*" element={<Page404 />} />
+                </Routes>
+            </BrowserRouter>
+        </Fragment>
     );
 };
 
